65c9d4a03729035216b5310b82b4cb23236c4cfb
Growlyj <296851292@qq.com>
2017-01-18 12:45:59 +0800

    YJ

--- HARDWARE/CC1101/cc1101.h
+++ HARDWARE/CC1101/cc1101.h
@@ -4,9 +4,10 @@
 #include "sys.h"
 #include "cc1101_REG.h"
 
-#define RX_Address 0xff                // 接收端 设备地址    1101网内设备地址
-
-#define TX_Address 0x01           // 发送端1 设备地址
+// Queen
+#define RX_Address 0xff           // 接收端 设备地址    1101网内设备地址
+// Drone
+#define TX_Address 0x01           // 发送端1 设备地址   
 //#define TX_Address 0x02         // 发送端2 设备地址
 //#define TX_Address 0x03         // 发送端3 设备地址
 //#define TX_Address 0x04         // 发送端4 设备地址
@@ -17,20 +18,13 @@
 #define TX 1                    // 发送
 #define RX 0                    // 接收
 #define ACK_LENGTH      4       // 应答信号长度       
-#define SEND_LENGTH     5      // 发送数据包长度
-#define SEND_GAP        1000    // 间隔1s发送一次数据
-#define RECV_TIMEOUT    1500    // 接收超时
+#define SEND_LENGTH     6       // 发送数据包长度
+#define RECV_TIMEOUT    3000    // 接收超时
 
 //#define WORK_MODE TX       // 发送
 #define WORK_MODE RX       // 接收
-/*===========================================================================
-------------------------------Internal IMPORT functions----------------------
-you must offer the following functions for this module
-1. uint8_t SPI_ExchangeByte(uint8_t input); // SPI Send and Receive function
-2. CC_CSN_LOW();                            // Pull down the CSN line
-3. CC_CSN_HIGH();                           // Pull up the CSN Line
-===========================================================================*/
-// CC1101,  GDO2(PA3),  CSN(PA4),  GDO0/IRQ(PA8),
+
+// CC1101数据线         GDO2(PA3),  CSN(PA4),  GDO0/IRQ(PA8),
 #define PORT_CC_GDO2    GPIOA
 #define PIN_CC_GDO2     GPIO_Pin_3  
 
@@ -39,18 +33,18 @@ you must offer the following functions for this module
 
 #define PORT_CC_IRQ     GPIOC                   // 通过ADC采集该脚电压可测温度
 #define PIN_CC_IRQ      GPIO_Pin_4
-	                                                             //当CSN变低，MCU必须等待MISO脚变低（表明电压调制器已经稳定，晶体正在运作中）								
+	                                                            //当CSN变低，MCU必须等待MISO脚变低（表明电压调制器已经稳定，晶体正在运作中）								
 #define CC_CSN_LOW()    GPIO_ResetBits(PORT_CC_CSN, PIN_CC_CSN);while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6) != 0)   // SPI_MISO
 #define CC_CSN_HIGH()   GPIO_SetBits(PORT_CC_CSN, PIN_CC_CSN)
 
 #define CC_IRQ_READ()   GPIO_ReadInputDataBit(PORT_CC_IRQ, PIN_CC_IRQ)
 
-extern volatile u8 CC_IRQ_Flag;          // CC1101数据包接收信号标志
+extern volatile u8 CC1101_IRQFlag;          // CC1101数据包接收信号标志
 
 /*===========================================================================
 ----------------------------------macro definitions--------------------------
 ============================================================================*/
-typedef enum {TX_MODE, RX_MODE} TRMODE;
+typedef enum {TX_MODE,   RX_MODE} TRMODE;
 typedef enum {BROAD_ALL, BROAD_NO, BROAD_0, BROAD_0AND255} ADDR_MODE;
 typedef enum {BROADCAST, ADDRESS_CHECK} TX_DATA_MODE;
 
@@ -58,23 +52,24 @@ typedef enum {BROADCAST, ADDRESS_CHECK} TX_DATA_MODE;
 -------------------------------------exported APIs---------------------------
 ============================================================================*/
 
-uint8_t CC1101ReadReg(uint8_t addr);    /*read a byte from the specified register*/
-uint8_t CC1101ReadStatus(uint8_t addr);/*Read a status register*/
-void CC1101WriteCmd(uint8_t command);/*Write a command byte to the device*/
-void CC1101SetIdle(void);  /*Set the CC1101 into IDLE mode*/
-void CC1101SendPacket(uint8_t *txbuffer, uint8_t size, TX_DATA_MODE mode); /*Send a packet*/
-void CC1101SetAddress(uint8_t address, ADDR_MODE AddressMode); /*Set the address and address mode of the CC1101*/
-void CC1101SetSYNC(uint16_t sync); /*Set the SYNC bytes of the CC1101*/
-uint8_t CC1101RecPacket(uint8_t *rxBuffer);/*Receive a packet*/
-void  CC1101WORInit(void);/*Initialize the WOR function of CC1101*/
-void CC1101Init(void);    /*Initialize the CC1101, User can modify it*/
+uint8_t CC1101ReadReg(uint8_t addr);    // read a byte from the specified register
+uint8_t CC1101ReadStatus(uint8_t addr); // Read a status register
+void CC1101WriteCmd(uint8_t command);   // Write a command byte to the device
+void CC1101SetIdle(void);               // Set the CC1101 into IDLE mode
+void CC1101SendPacket(uint8_t *txbuffer, uint8_t size, TX_DATA_MODE mode); // Send a packet
+void CC1101SetAddress(uint8_t address, ADDR_MODE AddressMode);             // Set the address and address mode of the CC1101
+void CC1101SetSYNC(uint16_t sync);             // Set the SYNC bytes of the CC1101
+uint8_t CC1101RecPacket(uint8_t *rxBuffer);    // Receive a packet
+void CC1101WORInit(void);               // Initialize the WOR function of CC1101
+void CC1101Init(void);                  // Initialize the CC1101, User can modify it
 
-uint8_t SPI_ExchangeByte(uint8_t Data);
-void  CC1101SetTRMode(TRMODE mode);      // Set the device as TX mode or RX mode*/
+uint8_t SPI_ExchangeByte(uint8_t Data);  // SPI读写数据
+void  CC1101SetTRMode(TRMODE mode);      // Set the device as TX mode or RX mode
 uint8_t Get_1101RSSI(void);              // 获取RSSI值
-
+void CC1101_Settings(void); 
+	
 void EXTI4_Set(u8 en);                   // EXTI4外部中断开关  en:1,使能; 0,屏蔽;  
-void TIM3_Set(u8 sta);
+void TIM3_Set(u8 sta);                   // TIM3的开关        sta:0,关闭; 1,开启;
 void TIM3_Init(u16 arr,u16 psc);
 	
 #endif // _CC1101_H_
--- USER/main.c
+++ USER/main.c
@@ -19,38 +19,47 @@ const u32 Queen_ID = 0x12131415;         // 32位ID
 volatile u16 Time_1ms = 0;               // 1ms计数器  接收方应答超时检测
 volatile int RecvWaitTime = 0;           // 接收等待超时时间
 
-u8 Link_Flag = 0;                        // 标记已建立过连接，可直接连入上次选择的wifi
+u8 Wifi_LinkFlag = 0;                    // 标记已建立过连接，出错重连时可直接连入上次选择的wifi
 
-                           // 帧头  源地址  目标地址 distance*10  帧尾
-u8 SendBuffer[SEND_LENGTH] = {0x55,   0,    0xff,    15,          0xaa};  // 从机待发送数据
+                           // 帧头  源地址  目标地址 distance*10  电量百分比 帧尾
+u8 SendBuffer[SEND_LENGTH] = {0x55,   0,    0xff,    15,          50,        0xaa};  // 从机待发送数据
                            // 帧头  源地址  目标地址  帧尾
-u8 AckBuffer[ACK_LENGTH]   = {0x55,  0xff,     0,     0xaa};              // 主机应答数据
+u8 AckBuffer[ACK_LENGTH]   = {0x55,  0xff,     0,     0xaa};                         // 主机应答数据
 
-volatile u8 Str_Info[20];  // 从机数据包中的 有效数据
+volatile u8 Str_Info[20];  // TCP服务器数据包。包含从机数据包中的有效数据
 
 u8 RF_SendPacket(uint8_t *Sendbuffer, uint8_t length);     // 无线发送数据函数  
 u8 RF_RecvHandler(void);                                   // 无线数据接收处理 
-void Sys_Init(void);                                       // Oil_Can Queen 系统初始化 
-
+void Sys_Init(void);                                       // Oil_Can Queen 系统外设初始化
 
 int main(void)
 { 
-	u8 Link_Error = 0, res = 0;
+	u8 Wifi_SendError = 0, res = 0;           // Wifi_SendError:wifi模块向服务器发送数据出错次数   res：Queen CC1101接受结果  
 	
-	Sys_Init();
+	Sys_Init();                               // Oil_Can Queen 系统外设初始化 
+	
 	while(1)
 	{
 		res = RF_RecvHandler();                                 // 无线数据接收处理 
-		if(res != 0) printf("Rec ERROR:%d\r\n", (int)res);      // 接收错误
+		if(res != 0) 
+		{
+			printf("Rec ERROR:%d\r\n", (int)res);               // 接收错误
+			LCD_Fill(0,271,239,295,WHITE);
+			LCD_ShowString(84,275,72,16,16, (u8*)"Rec ERROR");
+			LCD_ShowxNum(156,275,res,1,16,0);     
+		}	
 		else                                                    // 接收成功
 		{
 			while(1)    // 持续发送，直到发送成功！
 			{
 				if(atk_8266_wifisend_data((u8*)Str_Info) == 0)      // wifi发送失败
 				{
-					if(++Link_Error == 3)     // 连续3次发送失败，重新连接TCP
+					LCD_Fill(0,271,239,295,WHITE);
+					LCD_ShowString(44,275,152,16,16, (u8*)"Device#  Send ERROR");
+					LCD_ShowxNum(100,275,Str_Info[5],1,16,0);       // Device number   Drone_ID
+					if(++Wifi_SendError == 3)     // 连续3次发送失败，重新连接TCP
 					{
-						Link_Error = 0;       // 清零
+						Wifi_SendError = 0;       // 清零
 						printf("连接出错，正在重新连接...\r\n\r\n");	
 						atk_8266_init();      // ATK-ESP8266模块初始化配置函数
 						CC1101Init(); 
@@ -60,7 +69,10 @@ int main(void)
 				}
 				else                                                // wifi发送成功
 				{
-					Link_Error = 0;      // 清零
+					LCD_Fill(0,271,239,295,WHITE);
+					LCD_ShowString(44,275,152,16,16, (u8*)"Device#  Send OK!");
+					LCD_ShowxNum(100,275,Str_Info[5],1,16,0);       // Device number   Drone_ID
+					Wifi_SendError = 0;      // 清零
 					printf("wifi发送成功\r\n\r\n");
 					break;
 				}
@@ -68,19 +80,6 @@ int main(void)
 		}
 	}
 }
-
-// wifi测试程序
-//	while(1)    // 持续发送，直到发送成功！
-//	{
-//		res = atk_8266_wifisend_data((u8*)"wifi test");
-//		if(res) printf("send ok\r\n");
-//		else    
-//		{
-//			printf("send error\r\n");
-//			atk_8266_init();                                        // ATK-ESP8266模块初始化配置函数
-//		}
-//		delay_ms(500);delay_ms(500);delay_ms(500);
-//	}
 
 /*===========================================================================
 * 函数 : RF_SendPacket() => 无线发送数据函数                            *
@@ -92,42 +91,52 @@ int main(void)
 		 4：数据包源地址错误        
 		 5：数据包目标地址错误
 		 6：数据包帧尾错误
-
+         7: 应答信号错误
 ============================================================================*/
 uint8_t RF_SendPacket(uint8_t *Sendbuffer, uint8_t length)
 {
-	uint8_t  i = 0, ack_len = 0, ack_buffer[65] = {0};
+	uint8_t  i = 0, ack_len = 0, ack_buffer[10] = {0};
+	RecvWaitTime = (int)RECV_TIMEOUT;           // 等待应答超时限制1500ms
 	
 	CC1101SendPacket(SendBuffer, length, ADDRESS_CHECK);    // 发送数据   
-	                       
-	CC1101Init();                               // 初始化L01寄存器    
 	CC1101SetTRMode(RX_MODE);                   // 设置RF芯片为接收模式,接收数据
-
-	RecvWaitTime = (int)RECV_TIMEOUT;           // 等待应答超时限制1500ms
+	
 	TIM3_Set(1);                                // 开启定时器TIM3
-
-	printf("Send Over, waiting for ack...\r\n");
+	//printf("waiting for ack...\r\n");
 	while(CC_IRQ_READ() != 0)                   // 等待接收数据包
 	{
 		if(RecvWaitTime <= 0)      
 		{  
-			TIM3_Set(0);                            // 关闭定时器TIM3
-			return 1;                               // 等待应答超时
+			TIM3_Set(0);                        // 关闭定时器TIM3
+			return 1;                           // 等待应答超时
 		}
 	}
-	while(CC_IRQ_READ() == 0);               
+	
+	RecvWaitTime = 50;                          // 等待应答超时限制50ms
+	while(CC_IRQ_READ() == 0)
+    {
+        if(RecvWaitTime <= 0)      
+        {  
+            TIM3_Set(0);                         // 关闭定时器TIM3
+            return 7;                            // 应答信号错误
+        }
+    }               
 	
 	TIM3_Set(0);                                // 关闭定时器TIM3
 	ack_len = CC1101RecPacket(ack_buffer);      // 读取收到的数据
 	
-//	                                 //帧头  源地址  目标地址  帧尾2B
-//uint8_t AckBuffer[ACK_LENGTH]   = {0x55,  0xff,     0,     0x0d, 0x0a};    // 主机应答数据
+	if(ack_len <= 0 || ack_len > 10)  
+    {
+        CC1101Init(); 
+        //printf("ack_len1=%d\r\n", ack_len);
+        return 2;                               // 数据包长度错误
+    }
 	
 	if(ack_len != ACK_LENGTH) return 2;                                               // 数据包长度错误
 	if(ack_buffer[0] != 0x55) return 3;                                               // 数据包帧头错误
 	if(ack_buffer[1] != 0xff) return 4;                                               // 数据包源地址错误        
-	if(ack_buffer[2] == 0xff) return 5;                                               // 数据包目标地址错误     非法地址（0xff为主机地址！！）
-	if((ack_buffer[ack_len-2] != 0x0d) || (ack_buffer[ack_len-1] != 0x0a)) return 6;  // 数据包帧尾错误
+	if(ack_buffer[2] != TX_Address) return 5;                                         // 数据包目标地址错误     非法地址（0xff为主机地址！！）
+	if(ack_buffer[3] != 0xaa) return 6;  // 数据包帧尾错误
 
 	// 应答正确
 	printf("ack_len=%d;ack_buffer:", (int)ack_len);
@@ -153,27 +162,25 @@ uint8_t RF_SendPacket(uint8_t *Sendbuffer, uint8_t length)
 ============================================================================*/
 uint8_t RF_RecvHandler(void)
 {
-	uint8_t i = 0, length = 0;    // recv_buffer[10] = {0};
+	uint8_t i = 0, length = 0;    
 	u8 *recv_buffer = mymalloc(10);	     // 申请10字节内存
-	u16 Wait_Timer = 0;
+	u16 Wait_Timer = 0;                  // CC1101接收数据等待时间
 	
 	for(i=0; i<SEND_LENGTH; i++) recv_buffer[i] = 0;  // 数据清零,防止误判
 	RecvWaitTime = 40;                   // 等待应答超时限制40ms  正常情况10ms之内会完成
+	CC1101_IRQFlag = 0;
 	
 	CC1101SetTRMode(RX_MODE);            // 接收模式 
+	EXTI4_Set(1);                        // 使能EXTI4中断
 	
-	CC_IRQ_Flag = 0;
-	EXTI4_Set(1);                        // 使能EXTI8中断
-	//while(CC_IRQ_READ() != 0);           // 等待接收数据包
-	//printf("waiting1...\r\n");
 	while(1)
 	{
-		Wait_Timer++;
-		delay_us(300);
-		if(CC_IRQ_Flag) 
+		if(++Wait_Timer == 15000) LCD_Fill(0,271,239,295,WHITE); // 清除通信提示区
+		delay_us(200);
+		if(CC1101_IRQFlag)                  // CC1101接收到数据包信号
 		{
-			printf("2\r\n");
-			break;        // CC1101接收到数据包信号
+			//printf("2\r\n");
+			break;        
 		}
 		else
 		{
@@ -199,28 +206,17 @@ uint8_t RF_RecvHandler(void)
 		}
 	}
 	TIM3_Set(0);                                      // 关闭定时器TIM3
-	//printf("RecvWaitTime1=%d\r\n", RecvWaitTime);     // 正常情况10ms之内会完成
 	
 	length = CC1101RecPacket(recv_buffer);            // 读取接受到的数据长度和数据内容
-
-//                           // 帧头  源地址  目标地址 distance:1B) 帧尾
-//u8 SendBuffer[SEND_LENGTH] = {0x55,   0,    0xff,    15,          0xaa};  // 从机待发送数据
-//                           // 帧头  源地址  目标地址  帧尾2B
-//u8 AckBuffer[ACK_LENGTH]   = {0x55,  0xff,     0,     0xaa};         
-
-	//printf("strlen(recv_buffer)=%d\r\n", (int)strlen((const char*)recv_buffer));
-	
 	if(length <= 0 || length > 10)     // 数组越界处理
 	{
 		CC1101Init(); 
-		//printf("length1=%d\r\n", length);
-		myfree(recv_buffer);		//释放内存
-		return 7;                                              // 数据包长度错误
+		myfree(recv_buffer);		   // 释放内存
+		return 7;                      // 数据包长度错误
 	}
 	if(length != SEND_LENGTH)  
 	{
 		CC1101Init(); 
-		//printf("length2=%d\r\n", length);
 		myfree(recv_buffer);		// 释放内存
 		return 1;                   // 数据包长度错误
 	}
@@ -239,16 +235,41 @@ uint8_t RF_RecvHandler(void)
 		myfree(recv_buffer);		// 释放内存
 		return 4;                   // 数据包目标地址错误
 	}
-	if(recv_buffer[4] != 0xaa)
+	if(recv_buffer[5] != 0xaa)
 	{
 		myfree(recv_buffer);		// 释放内存
 		return 5;                   // 数据包帧尾错误
 	}
 	
-	// 数据接收正确  提取出从机数据包中的有效数据到  Str_Info 中    从机地址+有效数据
+	// 数据接收正确 LCD显示 
+	POINT_COLOR = RED;
+	switch(recv_buffer[1])         // 从机地址   Drone_ID
+	{
+		case 1:
+			LCD_ShowxNum(54, 72,recv_buffer[3],3,24,0);      // #1   Dis
+			LCD_ShowxNum(54,102,recv_buffer[4],3,24,0);      // Bat
+			break;
+		case 2:
+			LCD_ShowxNum(174, 72,recv_buffer[3],3,24,0);     // #2   Dis
+			LCD_ShowxNum(174,102,recv_buffer[4],3,24,0);     // Bat
+			break;
+		case 3:
+			LCD_ShowxNum(54,197,recv_buffer[3],3,24,0);      // #3   Dis
+			LCD_ShowxNum(54,227,recv_buffer[4],3,24,0);      // Bat
+			break;
+		case 4:
+			LCD_ShowxNum(174,197,recv_buffer[3],3,24,0);     // #4   Dis
+			LCD_ShowxNum(174,227,recv_buffer[4],3,24,0);     // Bat
+			break;
+		default:
+			break;
+	}
+	
+	//提取出从机数据包中的有效数据到  Str_Info 中    从机地址+有效数据
 	Str_Info[5] = recv_buffer[1];                // 从机地址   Drone_ID
-	Str_Info[6] = recv_buffer[3];                // Distance   单位：cm
-	Str_Info[7] = 0;                               // 添加字符串结束符
+	Str_Info[6] = recv_buffer[3];                // Distance         单位：cm
+	Str_Info[7] = recv_buffer[4];                // 电池电量百分比   [0,100]
+	Str_Info[8] = 0;                             // 添加字符串结束符
 
 	printf("Rec from:%d Distance:%dcm\r\n", (int)Str_Info[5], (int)Str_Info[6]);        // 显示从机地址
 	//printf("Str_length=%d; Str_Info:%s; RSSI=%d dB\r\n", (int)strlen((const char*)Str_Info), Str_Info, (int)Get_1101RSSI());
@@ -256,6 +277,7 @@ uint8_t RF_RecvHandler(void)
 	// 主机向从机回发应答信号    ！！！！！必须要，否则接收状态将接收不到数据！！！！！
 	CC1101SetTRMode(TX_MODE);  
 	AckBuffer[2] = Str_Info[5];        // 从机地址
+	delay_us(500);
 	CC1101SendPacket(AckBuffer, ACK_LENGTH, ADDRESS_CHECK);
 	
 	printf("Ack to %d OK\r\n", (int)Str_Info[5]);
@@ -264,45 +286,54 @@ uint8_t RF_RecvHandler(void)
 	return 0;
 }
 
+// Oil_Can Queen系统外设初始化
 void Sys_Init(void)
 {
 	u8 *ID;
-	
+
 	// 固定数据
 	Str_Info[0] = 0x23;                          // TCP包头
 	Str_Info[1] = (u8)(Queen_ID >> 24);          // Queen_ID
 	Str_Info[2] = (u8)(Queen_ID >> 16);          // Queen_ID
 	Str_Info[3] = (u8)(Queen_ID >> 8);           // Queen_ID
 	Str_Info[4] = Queen_ID & 0xff;               // Queen_ID
-	
+
 	delay_init();	    	                         // 延时函数初始化
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  // 设置NVIC中断分组2:2位抢占优先级，2位响应优先级	  
 	uart_init(115200);	 	                         // 初始化串口1波特率为115200	调试
 	USART2_Init(115200);                             // 初始化串口2波特率为115200   wifi
-	
+
 	LCD_Init();				                         // 初始化液晶 	
-	KEY_Init();                                      // LCD背光灯开关
-	LCD_ShowString(25,150,200,24,24,(u8*)"wifi scanning..."); 
+	KEY_Init();                                      // LCD背光灯开关键
 	tp_dev.init();			                         // 初始化触摸屏
-	
+
 	SPI1_Init();                                     // CC1101 SPI通信初始化
 	TIM3_Init(99,7199);		                         // CC1101 1ms中断
 	mem_init();                                      // 初始化内存池
-	
+
 	printf("Oil_Can_Queen\r\n");
-	atk_8266_init();                                 // ATK-ESP8266模块初始化配置函数	
-	printf("atk_8266_init OK!\r\n");
+	while(atk_8266_init());                          // 等待wifi、TCP连接成功	
 	ID = mymalloc(32);							     // 申请32字节内存
 	sprintf((char*)ID, "Queen_ID: %x Connected.", Queen_ID);
 	atk_8266_wifisend_data((u8*)ID);                 // 传送Queen_ID到TCP服务器
 	myfree(ID);		                                 // 释放内存 
-	printf("Mode:RX\r\n");
- 
+
 	CC1101Init(); 
 }
 
 
-
+// wifi发送  测试程序
+//	while(1)    // 持续发送，直到发送成功！
+//	{
+//		res = atk_8266_wifisend_data((u8*)"wifi test");
+//		if(res) printf("send ok\r\n");
+//		else    
+//		{
+//			printf("send error\r\n");
+//			atk_8266_init();                                        // ATK-ESP8266模块初始化配置函数
+//		}
+//		delay_ms(500);delay_ms(500);delay_ms(500);
+//	}
 
 
 
